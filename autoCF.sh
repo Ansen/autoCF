1b56e2c2b08a6b59cedd3980279381d9848d4e2d
An <shen.an89@gmail.com>
2025-05-08 15:45:10 +0800

    Update autoCF.sh

--- autoCF.sh
+++ autoCF.sh
@@ -84,7 +84,7 @@ function install_gost(){
     wget -q --show-progress -O "${tmp_file}" https://github.com/ginuerzh/gost/releases/download/v${version}/gost_${version}_${os}_${bit}.tar.gz
     [[ $? -ne 0 ]] && echo "gost下载失败" && exit 1
     [[ ! -f ${tmp_file} ]] && echo "gost下载失败" && exit 1
-    gzip -d ${tmp_file}
+    tar xf ${tmp_file}
     [[ $? -ne 0 ]] && echo "gost解压失败" && exit 1
     chmod +x gost
     mv gost "${gost_install_path}/"
